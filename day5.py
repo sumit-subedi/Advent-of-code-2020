5331104c9b6c37fc4ddbfbc071c9f367394f9347
sumit-subedi <vanje.sumit@gmail.com>
2020-12-11 16:25:48 +0545

    day 5 code

--- day5.py
+++ day5.py
@@ -3,17 +3,62 @@ import math
 
 file = open('day5.txt', 'r')
 
-string = file.read().split('\n\n')
+string = file.read().split('\n')
 nums = []
+
+
+#part 1 solution
+# for seat in string:
+#   low = 0
+#   high = 127
+#   row = 0
+#   column = 0
+
+#   for i in range(7):
+   
+#     if seat[i] == "F":
+#       high = int((low + high) / 2)
+
+#     elif seat[i] == "B":
+#       low = int((high - low) / 2)
+#       low = high - low
+  
+
+#   row = high
+#   low = 0
+#   high = 7
+
+  
+
+#   for i in range(7, len(seat)):
+#     if seat[i] == "L":
+#       high = int((low + high) / 2)
+
+#     elif seat[i] == "R":
+#       low = int((high - low) / 2)
+#       low = high - low
+    
+#   column = high
+#   nums.append(row * 8 + column)
+  
+
+# print(max(nums))
+
+###  End of part 1 solution
+
+
+### Part 2 solution
+
+
+
 for seat in string:
-  print(seat)
   low = 0
   high = 127
   row = 0
   column = 0
 
   for i in range(7):
-    print("here")
+   
     if seat[i] == "F":
       high = int((low + high) / 2)
 
@@ -37,7 +82,14 @@ for seat in string:
       low = high - low
     
   column = high
+  if row == 0 or row == 127:
+    continue
   nums.append(row * 8 + column)
-  print(row * 8 + column)
 
-print((nums))
+
+
+for i in range(min(nums) + 1, max(nums)):
+  if i not in nums:
+    break
+
+print(i)
